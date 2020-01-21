3a65e684a276d977feddcf24c474b4dac53e9f9b
KarimGil <karimg87@hotmail.com>
2020-01-21 03:56:41 -0800

    making frontend

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def blogHome(request):
+    return HttpResponse('all blogs here')
+
+def blogPost(request,slug):
+    return HttpResponse (f'Welcome to {slug}')
---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,'home/home.html')
+
+def contact(request):
+    return HttpResponse('contact')
+
+def about(request):
+    return HttpResponse('about')
