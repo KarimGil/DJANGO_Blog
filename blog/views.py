5eab4eb478e8026164b7af8626cb9e0f230eee7d
KarimGil <karimg87@hotmail.com>
2020-01-23 01:44:58 -0800

    Basic functionality done

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, HttpResponse
+from .models import BlogPost
 
 # Create your views here.
 
 
 def blogHome(request):
-    return render(request,"blog/blogHome.html")
+    allPost = BlogPost.objects.all()
+    context = {'allPost': allPost}
+    return render(request,"blog/blogHome.html",context)
 
 def blogPost(request,slug):
-    return render(request,"blog/blogPost.html")
+    post = BlogPost.objects.filter(slug=slug).first()
+    context = {'post':post}
+    return render(request,"blog/blogPost.html",context)
