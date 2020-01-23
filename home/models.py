b3793537a28b51e86b08a6673b65a7af5ad6d3a6
KarimGil <karimg87@hotmail.com>
2020-01-22 23:05:09 -0800

    setting up contact table

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    contact = models.CharField(max_length=15)
+    message = models.TextField()
+    timeStamp = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
