61040a716683f4726bc3f23b93a1319b01c3a2e1
KarimGil <karimg87@hotmail.com>
2020-01-25 06:42:58 -0800

    Frontend

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, HttpResponse
 from .models import Contact
 from django.contrib import messages
-
+from blog.models import BlogPost
 # Create your views here.
 def home(request):
+    
     return render(request,'home/home.html')
 
 def contact(request):
@@ -24,4 +25,9 @@ def about(request):
     return render(request,'home/about.html')
 
 def search(request):
-    return HttpResponse('This is search')
+    query = request.GET['query']
+
+    #allPost = BlogPost.objects.all()
+    allPost = BlogPost.objects.filter(title__icontains=query)
+    params = {'allPost':allPost}
+    return render(request,'home/search.html',params)
